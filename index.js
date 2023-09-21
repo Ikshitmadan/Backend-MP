515f4e61b2f789989cf510edf294e993876e6cb4
Divyang Dheer <88249922+ddheer29@users.noreply.github.com>
2023-09-21 17:13:40 +0530

    mvc

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
 dotenv.config()
 
+// db config
+connectDB();
+
 const app = express();
+
+
+// middlewares
+app.use(express.json());
 
 
 
