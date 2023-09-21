1ef5abd9c462b2a7c304fedda1306f48215d40a1
pradyumnesos <pradyumnaojha856@gmail.com>
2023-09-22 01:19:18 +0530

    Fixed bugs

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,7 +1,7 @@
 var jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-  const token = req.header("auth-token");
+  const token = req.header("token");
   if (!token) {
     res.status(401).send({ error: "Please authenticate using a valid token " });
   }
